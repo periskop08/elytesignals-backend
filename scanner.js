176da51e517933dfca5795a65f7d7b1007aec868
Burhan Seçkin <periskop@Burhan-MacBook-Air.local>
2026-04-17 20:29:02 +0300

    Auto-Memory Sync & Manifesto Update: Fri Apr 17 20:29:02 +03 2026

--- scanner.js
+++ scanner.js
@@ -791,7 +791,7 @@ async function analyzeCoin(symbolInfo) {
         }
 
         // HACİM & LİKİDİTE KORUMASI (Demir Bey'in Mirası)
-        if (direction === 'LONG' && globalVol < 1700000) {
+        if (direction === 'LONG' && globalVol < 1500000) {
             console.log(`[VETO-VOL] ${sym} -> Hacim çok düşük (LONG: ${globalVol})`);
             return null;
         }
